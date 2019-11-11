7e9ca6b981ea970268dc9f16e3d3ef0f4a49fa20
Ryan McCartan <ryanjmccartan@gmail.com>
2019-11-11 13:49:33 -0600

    created structure for step 3 admin

--- src/components/Step3/Step3Admin.js
+++ src/components/Step3/Step3Admin.js
@@ -6,13 +6,27 @@ class Step3Admin extends Component {
     render() {
         return (
             <div>
+                <div className="showUser">
+                    Current User:
+                </div>
+
+                <div className="adminCriteria">
+                    User's criteria:
+                </div>
+
+                <div className="adminRequestShowing">
+                    Recent showing requested:
+                </div>
+
+                <button>Mark Complete</button>
             </div>
         );
     }
 }
 
 const mapStateToProps = state => ({
-    errors: state.errors,
+    criteria: state.criteria,
+    showing: state.showing
 });
 
 export default connect(mapStateToProps)(Step3Admin);

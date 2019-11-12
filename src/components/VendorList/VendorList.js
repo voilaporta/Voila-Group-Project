28ed0375fc1f2c4231d2251787edabf9bf1da051
See Yang <seeyang24@gmail.com>
2019-11-12 15:01:04 -0600

    added vendor, client, and admin list docs to dashboard

---
+++ src/components/VendorList/VendorList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class VendorList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>In Vendor List</h1>
+            </div>
+        )
+    }
+}
+
+export default VendorList;

3979d9522599abb0ab4cf95b0fc548c5624e518d
See Yang <seeyang24@gmail.com>
2019-11-13 10:58:50 -0600

    dialog box is up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,7 +24,6 @@ import './App.css';
 import DashboardAdmin from '../Dashboard/DashboardAdmin';
 import AppTopBar from '../AppTopBar/AppTopBar';
 import ProfileAdmin from '../Profile/ProfileAdmin';
-import ClientList from '../ClientList/ClientList';
 
 class App extends Component {
   componentDidMount () {
@@ -80,11 +79,6 @@ class App extends Component {
                 exact
                 path="/profile"
                 component={ProfileAdmin}
-              />
-              <ProtectedRoute
-                exact
-                path="/client"
-                component={ClientList}
               />
               {/* If none of the other routes matched, we will show a 404. */}
               <Route render={() => <h1>404</h1>} />

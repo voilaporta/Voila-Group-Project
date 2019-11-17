5ebd2a82d98112ff34d5edd97ef8d77513158964
Mike Lynch <mjlynch2@gmail.com>
2019-11-17 12:02:08 -0600

    show client's buyer journey status on the dashboard (ie steps complete/11)

--- src/components/Dashboard/DashboardUser.js
+++ src/components/Dashboard/DashboardUser.js
@@ -6,34 +6,59 @@ import { withRouter } from 'react-router-dom'
 import homelogo from './HomePlaceholder.png'
 //this here will show the client's dashboard with their buying journey status.
 class DashboardUser extends Component {
+
+    state = {
+        stepsCompleted: 0
+    }
+
+    componentDidMount() {
+        this.props.dispatch({ type: 'GET_JOURNEY'})
+        setTimeout(() => {
+            this.getStatusPercentage()
+        }, 200);
+    }
+
+    getStatusPercentage = () => {
+        if(this.props.userJourney.loading) {
+            return false;
+        }
+        else {
+            this.props.userJourney.forEach(step => 
+                {if(step.completed) {
+                    this.setState({stepsCompleted: this.state.stepsCompleted + 1})
+            }})
+        }
+    }
+
     journey = () => {
         this.props.history.push(`/buyer-journey/${this.props.user.id}`)
     }
     render() {
         return ( 
-        <div>
-        <h3>Your buying journey:</h3>
-            <div className="card">
-                <div  >
-                    <img className="cardImg" src={homelogo} alt="" />
-                    <div className="status" onClick={this.journey} >
-                        <h3 >Status</h3>
-                        <RadialProgress
-                            width={50}
-                            height={50}
-                            steps={10}
-                            step={1}
-                        />
+            <div>
+                <h3>Your buying journey:</h3>
+                    <div className="card">
+                        <div  >
+                            <img className="cardImg" src={homelogo} alt="" />
+                            <div className="status" onClick={this.journey} >
+                                <h3 >Status</h3>
+                                <RadialProgress
+                                    width={50}
+                                    height={50}
+                                    steps={10}
+                                    step={this.state.stepsCompleted}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            </div>
         )
     }
 }
 
 const mapStateToProps = state => ({
     user: state.user,
+    userJourney: state.userJourney,
 });
 
 // this allows us to use <App /> in index.js

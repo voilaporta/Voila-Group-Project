51782b52f19f1369ac0f73e76222706db81a7ba9
Mike Lynch <mjlynch2@gmail.com>
2019-11-18 09:51:33 -0600

    improve step 4 client display

--- src/components/Step4/Step4Client.js
+++ src/components/Step4/Step4Client.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import OfferAcceptedDisplay from './OfferAcceptedDisplay';
 
 class Step4Client extends Component {
 
     componentDidMount(){
         this.props.dispatch({type: 'GET_ACCEPTED_OFFER', payload: {userStepId: this.props.userStepId}})
+    }
+
+    matchOfferToUser = () => {
+        const offer = this.props.offerAccepted.find(offer => {return offer.userStepId === this.props.userStepId});
+        return offer;
     }
 
     render() {
@@ -17,13 +23,7 @@ class Step4Client extends Component {
                         {this.props.offerAccepted.length === 0 ? 
                             <p>No offers have been accepted at this time.</p> 
                         :
-                            <>
-                                <pre>{JSON.stringify(this.props.offerAccepted)}</pre>
-                                Congrats! Your offer of {this.props.offerAccepted.price} with {this.props.offerAccepted.downpayment} was accepted!
-                                <strong>NOTE:</strong> Please check back frequently as there will be serveral time-sensitive steps to follow.
-                                <br/>
-                                {this.props.offerAccepted.address}
-                            </>
+                            <OfferAcceptedDisplay offer={this.props.offerAccepted.find(offer => { return offer.userStep_id === this.props.userStepId })}/>
                         }
                     </>
                 } 
@@ -34,6 +34,7 @@ class Step4Client extends Component {
 
 const mapStateToProps = state => ({
     offerAccepted: state.offerAccepted,
+
 });
 
 export default connect(mapStateToProps)(Step4Client);

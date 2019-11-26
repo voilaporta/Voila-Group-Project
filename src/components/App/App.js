1fb1ed86a975707a54604fce498b179c980da2af
Mike Lynch <mjlynch2@gmail.com>
2019-11-26 09:33:38 -0600

    add routing to register page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,6 +23,7 @@ import ProfileAdmin from '../Profile/ProfileAdmin';
 import UpdateClient from '../Dashboard/Client/UpdateClient';
 import UpdateAdmin from '../Dashboard/Admin/UpdateAdmin';
 import UpdateVendor from '../Dashboard/Vendor/UpdateVendor';
+import Register from '../Register/Register';
 
 class App extends Component {
   componentDidMount() {
@@ -35,44 +36,32 @@ class App extends Component {
         <Theme>
           <div>
             <AppTopBar />
+
             <Switch>
               {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
               <Redirect exact from="/" to="/home" />
-           
+
               {/* For protected routes, the view could show one of several things on the same route.
               Visiting localhost:3000/home will show the UserPage if the user is logged in.
               Even though it seems like they are different pages, the user is always on localhost:3000/home
               User will be directed to either Client (UserPage) or Admin page (DashboardAdmin) */}
-              {this.props.user.role_id === 3 ?
-                <ProtectedRoute
-                  exact
-                  path="/home"
-                  component={UserPage}
-                />
-                :
-                <ProtectedRoute
-                  exact
-                  path="/home"
-                  component={DashboardAdmin}
-                />
-              }
+              {this.props.user.role_id === 3 ? (
+                <ProtectedRoute exact path="/home" component={UserPage} />
+              ) : (
+                <ProtectedRoute exact path="/home" component={DashboardAdmin} />
+              )}
 
+              {/* IMPORTANT! Comment out this line if you are running a production build! */}
+              <Route exact path="/register" component={Register} />
               {/* This works the same as the other protected route, except that if the user is logged in,
               they will see the info page instead. */}
+              <ProtectedRoute exact path="/info" component={InfoPage} />
               <ProtectedRoute
                 exact
-                path="/info"
-                component={InfoPage}
-              />
-              <ProtectedRoute
-                exact path="/buyer-journey/:id"
+                path="/buyer-journey/:id"
                 component={BuyerJourney}
               />
-              <ProtectedRoute
-                exact
-                path="/profile"
-                component={ProfileAdmin}
-              />
+              <ProtectedRoute exact path="/profile" component={ProfileAdmin} />
               <ProtectedRoute
                 exact
                 path="/updateclient/:id"
@@ -89,7 +78,6 @@ class App extends Component {
                 component={UpdateVendor}
               />
 
-
               {/* If none of the other routes matched, we will show a 404. */}
               {/* If none of the other routes matched, we will show a 404. */}
               <Route render={() => <h1>404</h1>} />
@@ -97,7 +85,7 @@ class App extends Component {
           </div>
         </Theme>
       </Router>
-    )
+    );
   }
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -10,6 +10,8 @@ import SaveIcon from '@material-ui/icons/Save';
 
 import Swal from 'sweetalert2';
 
+// This component should be disabled for any live production build. 
+// It is only to be used to create the first user if you are spinning up this project from scratch.
 const styles = theme => ({
     formControl: {
         margin: theme.spacing(1),
@@ -36,7 +38,7 @@ class Register extends Component {
         email: '',
         dropboxUrl: '',
         agent_id: null,
-        role_id: '',
+        role_id: 1,
         journey: false,
     }
 
@@ -61,7 +63,7 @@ class Register extends Component {
             email: '',
             dropboxUrl: '',
             agent_id: null,
-            role_id: '',
+            role_id: 1,
             journey: false,
         });
         Swal.fire(
@@ -77,73 +79,54 @@ class Register extends Component {
 
         return (
             <div>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            label="Username"
-                            type="text"
-                            fullWidth
-                            value={this.state.username}
-                            onChange={this.handleChange('username')}
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            label="Password"
-                            type="password"
-                            fullWidth
-                            value={this.state.password}
-                            onChange={this.handleChange('password')}
-                        />
-                        <TextField
-                            autoFocus
-                            label="First Name"
-                            type="text"
-                            fullWidth
-                            value={this.state.firstName}
-                            onChange={this.handleChange('firstName')}
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            label="Last Name"
-                            type="text"
-                            fullWidth
-                            value={this.state.lastName}
-                            onChange={this.handleChange('lastName')}
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            label="Email Address"
-                            type="email"
-                            fullWidth
-                            value={this.state.email}
-                            onChange={this.handleChange('email')}
-                        />
-                        <FormControl className={classes.formControl}>
-                            <InputLabel htmlFor="role_id">Select Admin Type</InputLabel>
-                            <Select
-                                value={this.state.role_id}
-                                onChange={this.handleChange('role_id')}
-                                inputProps={{
-                                    name: 'role_id',
-                                }}
-                            >
-                                <MenuItem value="">
-                                    <em>None</em>
-                                </MenuItem>
-                                <MenuItem value={1}>Agent</MenuItem>
-                                <MenuItem value={2}>Agent Team Member</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <Button onClick={this.props.handleClose} color="secondary" variant="outlined">
-                            Cancel
-                        </Button>
-                        <Button onClick={() => this.handleAddAdmin()} color="secondary" variant="contained">
-                            <SaveIcon className={classes.leftIcon} />
-                            Add Admin
-                        </Button>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Username"
+                    type="text"
+                    fullWidth
+                    value={this.state.username}
+                    onChange={this.handleChange('username')}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    value={this.state.password}
+                    onChange={this.handleChange('password')}
+                />
+                <TextField
+                    autoFocus
+                    label="First Name"
+                    type="text"
+                    fullWidth
+                    value={this.state.firstName}
+                    onChange={this.handleChange('firstName')}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Last Name"
+                    type="text"
+                    fullWidth
+                    value={this.state.lastName}
+                    onChange={this.handleChange('lastName')}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                    value={this.state.email}
+                    onChange={this.handleChange('email')}
+                />
+                <Button onClick={() => this.handleAddAdmin()} color="secondary" variant="contained">
+                    <SaveIcon className={classes.leftIcon} />
+                    Register
+                </Button>
             </div>
         )
     }
@@ -153,4 +136,4 @@ const mapStateToProps = state => ({
     user: state.user,
 });
 
-export default withStyles(styles)(connect(mapStateToProps)(AddAdminDialog));
+export default withStyles(styles)(connect(mapStateToProps)(Register));
